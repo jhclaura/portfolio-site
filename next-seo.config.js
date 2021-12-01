7780490b9778122fc43b26eee587d0b3a9d8d061
jhclaura <linkinmonkey@gmail.com>
2021-11-30 22:04:30 -0500

    Use next-seo library

---
+++ next-seo.config.js
@@ -0,0 +1,22 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: 'https://www.jhclaura.com/',
+    site_name: 'Laura Juo-Hsin Chen',
+    description: `Laura Juo-Hsin Chen is an immersive experience developer, designer, and doodler from Taiwan. She is interested in weird human interactions and how they shift participants’ perspective.`,
+    images: [
+      {
+        url: 'https://www.jhclaura.com/laura_juo-hsin_chen.png',
+        width: 1435,
+        height: 802,
+        alt: 'Laura Juo-Hsin Chen',
+        type: 'image/jpeg',
+      },
+    ],
+  },
+  twitter: {
+    site: '@jhclaura',
+    cardType: 'summary_large_image',
+  },
+}
